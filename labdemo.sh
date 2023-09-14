2586df9414481c2582f3d8e983478121aa90b30c
PavaniKRao <kpavani.1811@gmail.com>
2023-09-14 05:51:22 +0000

    script

---
+++ labdemo.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+git clone --quiet https://github.com/PavaniKRao/OSPF_Orchestrator.git
+cd OSPF_Orchestrator
+
+Help()
+{
+   # Display Help
+   echo 
+   echo "Usage:      ./labdemo -FLAG"
+   echo
+   echo "Supported flags with this script are: [-h|n|i|b|r|c]"
+   echo "----------------------------------------------"
+   echo
+   echo "flags usage:"
+   echo "h    Print this Help."
+   echo "n    Create Docker networks"
+   echo "i    Inspect all docker networks"
+   echo "b    Build Docker Containers"
+   echo "r    Run Docker Containers"
+   echo "c    Checking running Docker Containers"
+   echo 
+   echo "----------------------------------------------"
+}
+
+DockerCreateNetwork()
+{
+    docker network prune -f
+    ./create_dockernetworks.sh
+}
+
+DockerInspectNetwork()
+{
+    ./inspect_dockernetworks.sh
+}
+
+DockerBuild()
+{
+    docker-compose build
+}
+
+DockerRun()
+{
+    docker-compose up
+}
+
+DockerCheck()
+{
+    docker ps
+}
+
+while getopts "hnibrc" flag; do
+    case "${flag}" in
+        h) 
+        Help
+        ;;
+        n)
+        DockerCreateNetwork
+        ;;
+        i) 
+        DockerInspectNetwork
+        ;;
+        b) 
+        DockerBuild
+        ;;
+        r)
+        DockerRun
+        ;;
+        c)
+        DockerCheck
+        ;;
+        *)
+        echo
+        echo "Incorrect flag!"
+        Help
+        ;;
+    esac
+done
+
+if [ $# -eq 0 ];
+then
+    echo 
+    echo "Please run with appropriate flag!"
+    Help
+    exit 0
+fi
