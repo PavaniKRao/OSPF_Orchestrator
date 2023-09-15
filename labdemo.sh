ca780e8bdc2b2ccd90a62d120a56244f45b201a6
PavaniKRao <kpavani.1811@gmail.com>
2023-09-15 02:04:06 +0000

    adding tmux

--- labdemo.sh
+++ labdemo.sh
@@ -20,10 +20,10 @@ Help()
    echo "h    Print this Help."
    echo "n    Create Docker networks"
    echo "i    Inspect all docker networks"
-   echo "b    Build Docker Containers"
-   echo "r    Run Docker Containers"
+   echo "b    Build Docker Containers for a 2 node and 3 router topology"
+   echo "r    Run Docker Containers to build a 2 node and 3 router topology"
    echo "c    Checking running Docker Containers"
-   echo "a    Add router4 in the topology"
+   echo "a    Add router4 to the existing 2 node and 3 router topology. You can check running containers again using the -c flag."
    echo 
    echo "----------------------------------------------"
 }
@@ -59,13 +59,27 @@ DockerCheck()
 
 AddRouter4()
 {
+    docker rm $(docker ps -a -q)
     docker pull pavani181/quagga_ubuntu20.04:1.0
-    
     docker run -itd --cap-add=ALL --network=net_14 --ip 20.0.5.20 --name router4 pavani181/quagga_ubuntu20.04:2.0 sh
-
     docker network connect --ip 20.0.6.10 net_43 router4
-
     docker start router4
+    tmux new-session -d -s "router4"
+}
+
+LaunchNodes()
+{
+    for i in a b
+    do 
+        tmux new-session -d -s "host$i"
+        tmux send -t host$i "docker exec -it $(sudo docker ps -aqf "name=host$i") /bin/bash" ENTER
+    done
+
+    for j in 1 2 3
+    do
+        tmux new-session -d -s "router$j"
+        tmux send -t router$j "docker exec -it $(sudo docker ps -aqf "name=router$j") /bin/bash" ENTER
+    done
 }
 
 while getopts "hnibrca" flag; do
